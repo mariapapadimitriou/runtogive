495e250168a077b3df896a2f6e2d8b2641848a52
mariaapapadimitriou <maria.papadimitriou@mail.utoronto.ca>
2022-01-27 22:00:09 -0500

    push 3

--- javascript.js
+++ javascript.js
@@ -6,3 +6,26 @@
 // Pink: #F0576B
 // Purple: #8C86AA
 // Blue: #46B1C9
+
+const navSlide = () => {
+    const burger = document.querySelector('.burger');
+    const nav = document.querySelector('.nav-links');
+    const navLinks = document.querySelectorAll('.nav-links li');
+
+    burger.addEventListener('click', () => {
+        nav.classList.toggle('nav-active');
+
+        navLinks.forEach((link, index) => { 
+            if(link.style.animation){
+                link.style.animation = '';
+            } else {
+                link.style.animation = `navLinkFade 0.5s ease forwards ${index/7 + 0.2}s`;
+            }
+        });
+
+        burger.classList.toggle('toggle');
+    });
+}
+
+
+navSlide();
