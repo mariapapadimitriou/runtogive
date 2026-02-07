0c3dd3f70f2d008128bb448d192936540415779a
mariaapapadimitriou <maria.papadimitriou@mail.utoronto.ca>
2026-02-07 12:50:07 -0500

    gallery added

--- javascript/javascript.js
+++ javascript/javascript.js
@@ -47,7 +47,7 @@ function setupSlideshow() {
 
     imageFiles.forEach(function (image) {
         const img = new Image();
-        img.src = "images/photos/" + image;
+        img.src = "images/photos/highlights/" + image;
         img.classList.add("slideshow-img");
         img.loading = "lazy";
         img.alt = "Run to Give event photo";
