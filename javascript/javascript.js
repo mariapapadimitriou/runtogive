2b5f8857850c190ea4e89d201f0d8063285a076b
mariaapapadimitriou <maria.papadimitriou@mail.utoronto.ca>
2026-02-09 22:17:08 -0500

    photos 2025

--- javascript/javascript.js
+++ javascript/javascript.js
@@ -109,7 +109,7 @@ function setupSlideshow() {
 
     imageFiles.forEach(function (image) {
         const img = new Image();
-        img.src = "images/photos/2024/2025/" + image;
+        img.src = "images/photos/2024/" + image;
         img.classList.add("slideshow-img");
         img.loading = "lazy";
         img.alt = "Run to Give event photo";
